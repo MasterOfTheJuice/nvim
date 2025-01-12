ec0a8f9769a812867a6530357cb3b846a284d082
Niv Rosenthal <niv@deepsea.team>
2025-01-12 23:47:28 +0200

    minimap

--- lua/motj/core/global.lua
+++
@@ -1,9 +0,0 @@
-function MOVE_RIGHT_IF_IN_TREE()
-	-- Check if the current buffer name contains "NvimTree"
-	if string.find(vim.api.nvim_buf_get_name(0), "NvimTree") then
-		-- If "NvimTree" is in the buffer name, move focus to the right window
-		vim.api.nvim_command("wincmd l")
-	end
-end
-
-KEYMAP_OPTIONS = "noremap = true, silent = true"
--- lua/motj/core/init.lua
+++ lua/motj/core/init.lua
@@ -1,4 +1,3 @@
 require("motj.core.options")
-require("motj.core.global")
 require("motj.core.keymaps")
 require("motj.core.filetypes")
